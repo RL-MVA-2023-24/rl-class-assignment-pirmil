140c7f49a843291830d473330b61795c6e5aa589
Timothee Seloi <timothee.s25@gmail.com>
2024-03-02 13:25:04 +0100

    reinforce agent

---
+++ src/reinforce_agent.py
@@ -0,0 +1,106 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import trange
+from torch.distributions import Categorical
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, state_dim, hidden_dim, nb_actions):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, nb_actions)
+
+    def forward(self, x: torch.Tensor):
+        if x.dim() == 1:
+            x = x.unsqueeze(dim=0)
+        x = F.relu(self.fc1(x))
+        action_scores = self.fc2(x)
+        return F.softmax(action_scores,dim=1)
+
+    def sample_action(self, x):
+        probabilities = self.forward(x)
+        action_distribution = Categorical(probabilities)
+        return action_distribution.sample().item()
+
+    def log_prob(self, x, a):
+        probabilities = self.forward(x)
+        action_distribution = Categorical(probabilities)
+        return action_distribution.log_prob(a)
+
+class ReinforceAgent:
+    def __init__(self, config, policy_network: nn.Module):
+        self.device = "cuda" if next(policy_network.parameters()).is_cuda else "cpu"
+        self.scalar_dtype = next(policy_network.parameters()).dtype
+        self.policy = policy_network
+        self.gamma = config['gamma'] if 'gamma' in config.keys() else 0.99
+        lr = config['learning_rate'] if 'learning_rate' in config.keys() else 0.001
+        self.optimizer = torch.optim.Adam(list(self.policy.parameters()),lr=lr)
+        self.nb_episodes = config['nb_episodes'] if 'nb_episodes' in config.keys() else 1
+
+    def greedy_action(self, x):
+        with torch.no_grad():        
+            probabilities = self.policy(torch.as_tensor(x))
+            action = torch.argmax(probabilities).item()
+            return action
+
+    def sample_action_and_log_prob(self, x):
+        probabilities = self.policy(torch.as_tensor(x))
+        action_distribution = Categorical(probabilities)
+        action = action_distribution.sample()
+        log_prob = action_distribution.log_prob(action)
+        return action.item(), log_prob
+    
+    def one_gradient_step(self, env):
+        # run trajectories until done
+        episodes_sum_of_rewards = []
+        log_probs = []
+        returns = []
+        for ep in range(self.nb_episodes):
+            x,_ = env.reset()
+            rewards = []
+            episode_cum_reward = 0
+            while(True):
+                a, log_prob = self.sample_action_and_log_prob(x)
+                y,r,d,_,_ = env.step(a)
+                log_probs.append(log_prob)
+                rewards.append(r)
+                episode_cum_reward += r
+                x=y
+                if d:
+                    # compute returns-to-go
+                    new_returns = []
+                    G_t = 0
+                    for r in reversed(rewards):
+                        G_t = r + self.gamma * G_t
+                        new_returns.append(G_t)
+                    new_returns = list(reversed(new_returns))
+                    returns.extend(new_returns)
+                    episodes_sum_of_rewards.append(episode_cum_reward)
+                    break
+        # make loss
+        returns = torch.tensor(returns)
+        log_probs = torch.cat(log_probs)
+        loss = -(returns * log_probs).mean()
+        # gradient step
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        return np.mean(episodes_sum_of_rewards)
+
+    def train(self, env, nb_rollouts):
+        avg_sum_rewards = []
+        for ep in trange(nb_rollouts):
+            avg_sum_rewards.append(self.one_gradient_step(env))
+        return avg_sum_rewards
+    
+
+    def act(self, state):
+        return self.greedy_action(state)
+    
+    def save(self, path):
+        torch.save(self.policy.state_dict(), path)
+
+    def load(self, path):
+        self.policy.load_state_dict(torch.load(path, map_location=self.device))
+        self.policy.eval()
--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 from env_hiv import HIVPatient
 from dqn_agent import TargetNetwork, DeepQAgent
+from reinforce_agent import ReinforceAgent, PolicyNetwork
 from replay_buffer import prefill_buffer
 
 env = TimeLimit(
@@ -15,36 +16,48 @@ env = TimeLimit(
 )  # The time wrapper limits the number of steps in an episode at 200.
 
 ##### HYPERPARAMETERS TO TUNE ######
-agent_name = 'DQN'
+agent_name = ['DQN', 'Reinforce'][0]
 
-train_max_episode = 4000
+if agent_name == 'DQN':
+    prefill_steps = 8000
 
-target_network_name = 'TargetNetwork'
-target_network_activation = nn.SiLU()
-target_network_hidden_dim = 512
-target_network_depth = 5
-target_network_normalization = None
+    train_max_episode = 4000
 
-prefill_steps = 8000
-prefill_with_random = True
+    target_network_name = 'TargetNetwork'
+    target_network_activation = nn.SiLU()
+    target_network_hidden_dim = 512
+    target_network_depth = 5
+    target_network_normalization = None
 
-config = {
-    'learning_rate': 0.001,
-    'gamma': 0.98,
-    'buffer_size': 1000000,
-    'epsilon_min': 0.01,
-    'epsilon_max': 1.,
-    'epsilon_decay_period': 10000,
-    'epsilon_delay_decay': 400,
-    'batch_size': 1024,
-    'gradient_steps': 2,
-    'update_target_strategy': 'ema', # or 'replace'
-    'update_target_freq': 600,
-    'update_target_tau': 0.001,
-    'criterion': nn.SmoothL1Loss(), # or nn.HuberLoss()
-    'monitoring_nb_trials': 50, 
-    'monitor_every': 50,
-}
+    config = {
+        'learning_rate': 0.001,
+        'gamma': 0.98,
+        'buffer_size': 1000000,
+        'epsilon_min': 0.01,
+        'epsilon_max': 1.,
+        'epsilon_decay_period': 10000,
+        'epsilon_delay_decay': 400,
+        'batch_size': 1024,
+        'gradient_steps': 2,
+        'update_target_strategy': 'ema', # or 'replace'
+        'update_target_freq': 600,
+        'update_target_tau': 0.001,
+        'criterion': nn.SmoothL1Loss(), # or nn.HuberLoss()
+        'monitoring_nb_trials': 50, 
+        'monitor_every': 50,
+    }
+elif agent_name == 'Reinforce':
+    nb_rollouts = 200
+
+    policy_network_name = 'PolicyNetwork'
+    policy_network_hidden_dim = 128
+
+    config = {
+        'gamma': 0.99,
+        'learning_rate': 0.01,
+        'nb_episodes': 10
+    }
+
 
 ##### FIXED PARAMETERS ######
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -58,17 +71,23 @@ config_constant = {
 
 config.update(config_constant)
 
-if target_network_name == 'TargetNetwork':
-    model = TargetNetwork(state_dim, target_network_hidden_dim, nb_actions, target_network_depth, target_network_activation, target_network_normalization).to(device)
-
 if agent_name == 'DQN':
+    if target_network_name == 'TargetNetwork':
+        model = TargetNetwork(state_dim, target_network_hidden_dim, nb_actions, target_network_depth, target_network_activation, target_network_normalization).to(device)
     agent = DeepQAgent(config, model)
+elif agent_name == 'Reinforce':
+    if policy_network_name == 'PolicyNetwork':
+        policy_network = PolicyNetwork(state_dim, policy_network_hidden_dim, nb_actions)
+    agent = ReinforceAgent(config, policy_network)
+
 
 class ProjectAgent:
     def __init__(self, project_agent_name=agent_name):
         self.agent_name = project_agent_name
-        if self.agent_name:
+        if self.agent_name=='DQN':
             self.agent = DeepQAgent(config, model)
+        elif self.agent_name=='Reinforce':
+            self.agent = ReinforceAgent(config, policy_network)
 
     def act(self, observation, use_random=False):
         return self.agent.act(observation)
@@ -91,30 +110,40 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = True
 
     # Print model configurations
-    print(f"Final number of episodes {train_max_episode}")
-    print(f"Prefill steps {prefill_steps}")
+    print(f"Agent: {agent_name}")
     print(config)
-    print(model)
+    
+    # Train the model
+    if agent_name == 'DQN':
+        print(f"Final number of episodes: {train_max_episode}")
+        print(f"Prefill steps: {prefill_steps}")
+        print(f"Target network:\n{model}")
 
-    # Pre-fill the buffer
-    prefill_buffer(env, agent, prefill_steps, random_samples=prefill_with_random)
+        # Pre-fill the buffer
+        prefill_buffer(env, agent, prefill_steps, random_samples=True)
 
-    ep_length, disc_rewards, tot_rewards, V0 = agent.train(env, train_max_episode)
-    agent.save(config['save_path'])
+        ep_length, disc_rewards, tot_rewards, V0 = agent.train(env, train_max_episode)
+
+        # Plots
+        plt.figure(figsize=(15, 5))
+        plt.plot(ep_length, label="Training episode length")
+        plt.legend()
+        plt.grid(True)
+        plt.savefig(f'{agent_name}_1.png')
+        plt.close()
+        plt.figure(figsize=(15, 5))
+        plt.plot(disc_rewards, label="MC eval of discounted reward")
+        plt.plot(tot_rewards, label="MC eval of total reward")
+        plt.plot(V0, label="Average $max_a Q(s_0)$")
+        plt.legend()
+        plt.grid(True)
+        plt.savefig(f'{agent_name}_2.png')
+        plt.close()
+
+    elif agent_name == 'Reinforce':
+        print(f"Nb rollouts: {nb_rollouts}")
+        print(f"Policy network:\n{policy_network}")
+        returns = agent.train(env, nb_rollouts)
+
     print(f"Successfully trained {agent_name}!")
 
-    plt.figure(figsize=(15, 5))
-    plt.plot(ep_length, label="Training episode length")
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f'{agent_name}_1.png')
-    plt.close()
-
-    plt.figure(figsize=(15, 5))
-    plt.plot(disc_rewards, label="MC eval of discounted reward")
-    plt.plot(tot_rewards, label="MC eval of total reward")
-    plt.plot(V0, label="Average $max_a Q(s_0)$")
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f'{agent_name}_2.png')
-    plt.close()
